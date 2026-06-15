ad91412d278d1e888c96ee2d386bbc709fbea40e
ashish-singh-29 <ashishsinghsh226@gmail.com>
2026-06-15 17:17:01 +0530

    all about datatype

---
+++ 02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3+3)  we are using nodejs not web browser
+
+// console.log( 3+3 ); console.log( "ashish")  //code readiabilty kmmm ho jata hai isse
+ 
+// Instead of that we use 
+console.log(3+3)
+console.log("ashish")
+
+//datatypes
+let Name = "Ashish" // string data type
+let age = 12   // Integer data type
+let eligible = true // boolean data type
+let outcome = null  // Object (but it is also a data type)
+let value = undefined // undefined data type
+
