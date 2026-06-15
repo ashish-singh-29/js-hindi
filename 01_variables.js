4db74c517b7120a580711150ff2eacc3c478523f
ashish-singh-29 <ashishsinghsh226@gmail.com>
2026-06-15 12:17:00 +0530

    Learning about Variables

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "Ashish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2  //not allowed
+accountEmail = "Nikil@gmail.com"
+accountPassword = "3534543"
+accountCity = "Patna"
+
+// console.log(accountEmail);// writing this again and again takes time so instead of doing this we can create a table
+// console.log(accountEma);
+// console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
