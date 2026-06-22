58a4bdc203914aa7eaac43fca987beb6f6835e52
ashish-singh-29 <ashishsinghsh226@gmail.com>
2026-06-22 17:29:14 +0530

    Some comparision

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,12 +1,31 @@
-let score = true;
+// let score = true;
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" => 33
 //"33abc" => NaN(Not a number)
+
+
+// **************** Operations *****************
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**5);
+
+let str1 = "Hello"
+let str2 = " Ashish"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log((3+4)*5%3)
